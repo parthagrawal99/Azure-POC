16fd1d9aceab3607c45bafa33aeed702b22d21c1
Parth <parth.agrawal@mattel.com>
2023-09-21 12:10:11 +0530

    initial commit

---
+++ POCGetTrigger/index.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    let res1 = await axios.get('https://cat-fact.herokuapp.com/facts/');
+
+    let data = res1.data;
+    console.log(data);
+    // const name = (req.query.name || (req.body && req.body.name));
+    // const responseMessage = name
+    //     ? "Hello----------, " + name + ". This HTTP triggered function executed successfully."
+    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        status: 200, /* Defaults to 200 */
+        // body: responseMessage,
+        // test: "test"
+        data: data
+    };
+}
