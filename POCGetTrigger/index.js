cd3c6a139f2adf28c54ead0ce1391ad9392c3985
Parth <parth.agrawal@mattel.com>
2023-09-25 11:50:11 +0530

    added axios call

--- POCGetTrigger/index.js
+++ POCGetTrigger/index.js
@@ -6,11 +6,11 @@ module.exports = async function (context, req) {
         let res1 = await axios.get('https://cat-fact.herokuapp.com/facts/');
     
         let data = res1.data;
-        console.log(data);
+        console.log(data, "------------- data");
         const name = (req.query.name || (req.body && req.body.name));
         const responseMessage = name
             ? "Hello----------, " + name + ". This HTTP triggered function executed successfully."
-            : "----------This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+            : "----------This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.-----------";
     
         context.res = {
             status: 200, /* Defaults to 200 */
@@ -23,8 +23,9 @@ module.exports = async function (context, req) {
         context.res = {
             status: 500, /* Defaults to 200 */
             // body: responseMessage,
-            test: "test"
-            // data: data
+            test: "test",
+            // data: data,
+            err:err
         };
     }
 }
