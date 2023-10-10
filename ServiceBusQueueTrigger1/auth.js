592e4e6bf380e0d15155831e3881660d57bdc414
Parth <parth.agrawal@mattel.com>
2023-10-10 10:41:39 +0530

    salesforce enspoint call

---
+++ ServiceBusQueueTrigger1/auth.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+// const {SecretClient} = require('@azure/keyvault-secrets');
+// const {DefaultAzureCredentail}= require('@azure/identity');
+
+
+// Salesforce conf
+const salesforceConfig = {
+    clientId: '3MVG95mg0lk4batgXsIfxmF4cCf2xt9s_4TOoW4cmSUM6DITFPZGEiwkbt5vhgmbjHfSjZVSTjbTMB.4Q4t2k',
+    clientSecret: 'AC95EECDF4C5968F1E65D9D976821333ECDA02F28101B0CFF56AFC0DAF9A593D',
+    refreshToken: '5Aep861sDdjizbO.v67LqnYf.ft9iRXFahTWmFnFE51_rEreGDFEfhUW25TKGNOSuGDq4e7jLcMOVcZaZLU3YRW',
+    accessToken: '',
+    tokenExpiration: 0,
+    instanceUrl: 'https://dream-nosoftware-499.my.salesforce.com',
+};
+
+
+//Az key vault
+// const kVName = 'az-kv-sales-poc';
+// const kVUri= 'https://az-kv-sales-poc.vault.azure.net/';
+
+// const kVClient = new SecretClient(kVUri,new DefaultAzureCredentail());
+
+
+async function getToken() {
+    
+    // if (salesforceConfig.tokenExpiration > Date.now()) {
+    //     return salesforceConfig.accessToken;
+    // } else {
+        try {
+            const response = await axios.post(`${salesforceConfig.instanceUrl}/services/oauth2/token`, null, {
+                params: {
+                    grant_type: 'refresh_token',
+                    client_id: salesforceConfig.clientId,
+                    client_secret: salesforceConfig.clientSecret,
+                    refresh_token: salesforceConfig.refreshToken,
+                },
+            });
+
+
+            // Update access token
+            salesforceConfig.accessToken = response.data.access_token;
+
+            salesforceConfig.tokenExpiration = Date.now() + response.data.expires_in * 1000;
+
+            return salesforceConfig.accessToken;
+        } catch (error) {
+            throw new Error(`Failed to refresh access token: ${error.message}`);
+        }
+    // }
+}
+
+
+module.exports = { getToken };
--- ServiceBusQueueTrigger1/index.js
+++ ServiceBusQueueTrigger1/index.js
@@ -1,3 +1,18 @@
+import {getToken} from './auth';
+const axios = require('axios');
 module.exports = async function(context, mySbMsg) {
     context.log('JavaScript ServiceBus queue trigger function processed message', mySbMsg);
+    let token = await getToken();
+    const response = await axios.post('https://dream-nosoftware-499.my.salesforce.com/services/data/v25.0/sobjects/Contact/', {
+        "FirstName":"Parth",
+        "LastName":"Agrawal",
+        "Email":"parthagrawaltest@gmail.com",
+        "Phone":"0099887766778"
+    }, {headers: {
+        Authorization: `Bearer ${token}`
+    }});
+
+    context.log(response.data, "------------------ response data")
+    // https://www.youtube.com/watch?v=nXRtJmcXScY&list=PLUGpDBDXjE931vW6uquJIOcaGaxAoP6gN&index=8
+    // https://beautifuldawndesigns.net/easy-sketch-ideas-beginners-can-draw/
 };
